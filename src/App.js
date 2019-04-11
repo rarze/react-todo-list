b52890cf5bcf3ad135a16f63e373cd0ccfadef83
Rodrigo Arze <rodrigo.arze@jalasoft.com>
2019-04-11 18:09:27 -0400

    added all in one todo-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {todoItems: this.props.initItems};
+  }
+
+  addItem(todoItem) {
+      this.state.todoItems.unshift({
+      index: this.state.todoItems.length + 1,
+      value: todoItem.newItemValue,
+      done: false
+    });
+    this.setState({todoItems: this.state.todoItems});
+  }
+
+  removeItem (itemIndex) {
+    this.state.todoItems.splice(itemIndex, 1);
+    this.setState({todoItems: this.state.todoItems});
+  }
+
+  markTodoDone(itemIndex) {
+    let todo = this.state.todoItems[itemIndex];
+    this.state.todoItems.splice(itemIndex, 1);
+    todo.done = !todo.done;
+    todo.done ? this.state.todoItems.push(todo) : this.state.todoItems.unshift(todo);
+    this.setState({todoItems: this.state.todoItems});
+  }
+
+  componentDidMount() {
+    this.refs.itemName.focus();
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const newItemValue = this.refs.itemName.value;
+
+    if(newItemValue) {
+      this.addItem({newItemValue});
+      this.refs.form.reset();
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div id="main">
+          <h1>Todo list</h1>
+          <ul className="list-group">
+            {this.props.initItems.map((item, index) => {
+              return (<li key={index} className="list-group-item ">
+              <div className={item.done ? "done" : "undone"}>
+              <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={() => this.markTodoDone(index)}></span>
+                {item.value}
+              <button type="button" className="close" onClick={() => this.removeItem(index)}>&times;</button>
+              </div>
+              </li>)
+            })}
+          </ul>
+          <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+            <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
+            <button type="submit" className="btn btn-default">Add</button>
+          </form>
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const todoItems = [
+    {
+        index: 1,
+        value: "learn react",
+        done: false
+    },
+    {
+        index: 2,
+        value: "Go shopping",
+        done: true
+    },
+    {
+        index: 3,
+        value: "buy flowers",
+        done: true
+    }];
+ReactDOM.render(<App initItems={todoItems}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
